adcb2514e3921ce55d22e3c7b67ed10a761b49ef
Swartex <andrey92.ua@gmail.com>
2023-07-20 00:20:53 +0300

    initial

---
+++ app/(site)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-200 text-green-500">
+      Home
+    </main>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Public_Sans } from 'next/font/google';
+
+const publicSans = Public_Sans({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Task Manager',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={publicSans.className}>{children}</body>
+    </html>
+  );
+}
