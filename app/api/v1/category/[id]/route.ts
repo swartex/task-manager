d55c624ce2564482d7bf8b4c1b6678f81a942299
Swartex <andrey92.ua@gmail.com>
2023-08-11 00:20:09 +0300

    added delete method

--- app/api/v1/category/[id]/route.ts
+++ app/api/v1/category/[id]/route.ts
@@ -5,18 +5,3 @@ interface IParams {
   categoryID: string;
 }
 
-export const DELETE = async (req: NextRequest) => {
-  const { category_id } = await req.json();
-  try {
-    const deletedCategory = await prismadb.category.delete({
-      where: {
-        id: category_id,
-      },
-    });
-
-    return NextResponse.json(deletedCategory);
-  } catch (e) {
-    console.log(e);
-    return new NextResponse("error", { status: 500 });
-  }
-};
--- app/api/v1/category/route.ts
+++ app/api/v1/category/route.ts
@@ -2,7 +2,6 @@ import { NextResponse, NextRequest } from "next/server";
 import prismadb from "@/app/libs/prismadb";
 import { Category } from "@/app/types";
 
-
 export const GET = async () => {
   try {
     const categories = await prismadb.category.findMany();
@@ -26,6 +25,25 @@ export const POST = async (req: NextRequest) => {
 
     return NextResponse.json(newCategory, { status: 201 });
   } catch (e) {
+    return new NextResponse("error", { status: 500 });
+  }
+};
+
+export const DELETE = async (req: NextRequest) => {
+  const { category_id } = await req.json();
+  try {
+    const deletedCategory = await prismadb.category.delete({
+      where: {
+        id: category_id,
+      },
+    });
+
+    return NextResponse.json({
+      success: true,
+      deletedCategoryId: category_id,
+    });
+  } catch (e) {
+    console.log(e);
     return new NextResponse("error", { status: 500 });
   }
 };
